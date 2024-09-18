81ae0f559000057a8867f8cfa6f50c19e58257ef
QuocAnh189 <anhquoc18092003@gmail.com>
2024-09-18 19:42:01 +0700

    update settings page

--- src/features/form/profile.form/index.tsx
+++ src/features/form/profile.form/index.tsx
@@ -121,12 +121,6 @@ const UpdateProfileForm = () => {
 
             <div className='flex justify-end gap-4.5'>
               <button
-                className='flex justify-center rounded border border-stroke px-6 py-2 font-medium text-black hover:shadow-1 dark:border-strokedark dark:text-white'
-                type='submit'
-              >
-                Cancel
-              </button>
-              <button
                 className='flex justify-center rounded bg-primary px-6 py-2 font-medium text-gray hover:bg-opacity-90'
                 type='submit'
               >
