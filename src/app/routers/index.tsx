8b7ae18f6d6381bf01add0b5e7c435c4bd11d028
QuocAnh189 <anhquoc18092003@gmail.com>
2024-09-18 17:40:36 +0700

    update sidebar

--- src/app/routers/index.tsx
+++ src/app/routers/index.tsx
@@ -1,71 +1,81 @@
+import { IconCategory } from '@shared/assets/svgs/ui/sidebar/IconCategory'
 import { IconDashboard } from '@shared/assets/svgs/ui/sidebar/IconDashboard'
+import { IconEvent } from '@shared/assets/svgs/ui/sidebar/IconEvent'
+import { IconOrder } from '@shared/assets/svgs/ui/sidebar/IconOrder'
+import { IconPayment } from '@shared/assets/svgs/ui/sidebar/IconPayment'
+import { IconProfile } from '@shared/assets/svgs/ui/sidebar/IconProfile'
+import { IconTransaction } from '@shared/assets/svgs/ui/sidebar/IconTransaction'
+import { IconUser } from '@shared/assets/svgs/ui/sidebar/IconUser'
 
 const routes = [
   {
-    name: 'MENU',
+    name: 'DASHBOARD',
     menuItems: [
       {
         icon: <IconDashboard className='fill-current' />,
         label: 'Dashboard',
-        route: '#',
-        children: [{ label: 'Overview', route: '/' }]
-      },
-      {
-        icon: <IconDashboard className='fill-current' />,
-        label: 'Dashboard',
-        route: '#'
-      },
-      {
-        icon: <IconDashboard className='fill-current' />,
-        label: 'Dashboard',
-        route: '#'
-      },
-      {
-        icon: <IconDashboard className='fill-current' />,
-        label: 'Dashboard',
-        route: '#',
-        children: [
-          { label: 'Form Elements', route: '/forms/form-elements' },
-          { label: 'Form Layout', route: '/forms/form-layout' }
-        ]
-      },
-      {
-        icon: <IconDashboard className='fill-current' />,
-        label: 'Dashboard',
-        route: '#'
-      },
-      {
-        icon: <IconDashboard className='fill-current' />,
-        label: 'Dashboard',
-        route: '#'
+        route: '/'
       }
     ]
   },
   {
-    name: 'OTHERS',
+    name: 'ECOMMERCE',
     menuItems: [
       {
-        icon: <IconDashboard className='fill-current' />,
-        label: 'Dashboard',
-        route: '#'
+        icon: <IconCategory className='fill-current' />,
+        label: 'Manage Categories',
+        route: '#',
+        children: [{ label: 'Category', route: '/category' }]
       },
       {
-        icon: <IconDashboard className='fill-current' />,
-        label: 'Dashboard',
+        icon: <IconEvent className='fill-current' />,
+        label: 'Manage Events',
         route: '#',
         children: [
-          { label: 'Alerts', route: '/ui/alerts' },
-          { label: 'Buttons', route: '/ui/buttons' }
+          { label: 'Products', route: '/events' },
+          { label: 'Ticket', route: '/ticket-event' },
+          { label: 'Product Reviews', route: '/reviews' }
         ]
       },
       {
-        icon: <IconDashboard className='fill-current' />,
-        label: 'Dashboard',
+        icon: <IconOrder className='fill-current' />,
+        label: 'Orders',
         route: '#',
         children: [
-          { label: 'Sign In', route: '/auth/signin' },
-          { label: 'Sign Up', route: '/auth/signup' }
+          { label: 'All Orders', route: '/all-orders' },
+          { label: 'All Pending Orders', route: '/pending-orders' },
+          { label: 'All Cancel Orders', route: '/cancel-orders' }
         ]
+      },
+      {
+        icon: <IconTransaction className='fill-current' />,
+        label: 'Transactions',
+        route: '/transaction'
+      },
+      {
+        icon: <IconPayment className='fill-current' />,
+        label: 'Payment',
+        route: '/payment'
+      }
+    ]
+  },
+  {
+    name: 'SETTINGS & MORE',
+    menuItems: [
+      {
+        icon: <IconUser className='fill-current' />,
+        label: 'User',
+        route: '#',
+        children: [
+          { label: 'Customer List', route: '/customer-list' },
+          { label: 'Admin List', route: '/admin-list' },
+          { label: 'Manage User', route: '/manage-user' }
+        ]
+      },
+      {
+        icon: <IconProfile />,
+        label: 'Profile',
+        route: '/profile'
       }
     ]
   }
